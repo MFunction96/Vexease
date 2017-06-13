8eb1b501a7e60a11459bbdb4e0ffeded42503013
MFunction <mfunction@cugb.edu.cn>
2017-06-13 09:37:38 +0800

    创建工程

--- CommunalComputerManager/MainForm.Designer.cs
+++ CommunalComputerManager/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CommunalComputerManager
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 353);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
